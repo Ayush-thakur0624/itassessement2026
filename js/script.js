06aef8dfaf165ed02e9305318a73ce9174100f39
KrishikaAryal <aryalkrishika@gmail.com>
2026-07-14 21:45:31 +0545

    Inital project setup

---
+++ js/script.js
@@ -0,0 +1,49 @@
+// Image Slider
+
+let images = ["images/mars.jpg", "images/colony.jpg", "images/rocket.jpg"];
+
+let index = 0;
+
+function changeImage() {
+  let slider = document.getElementById("slider");
+
+  if (slider) {
+    index++;
+
+    if (index >= images.length) {
+      index = 0;
+    }
+
+    slider.src = images[index];
+  }
+}
+
+setInterval(changeImage, 3000);
+
+// Contact Form Validation
+
+function validateForm() {
+  let name = document.getElementById("name").value;
+
+  let email = document.getElementById("email").value;
+
+  if (name == "" || email == "" || message == "") {
+    alert("Please fill all required fields!");
+
+    return false;
+  }
+
+  alert("Form submitted successfully 🚀");
+
+  return true;
+}
+
+function showDate() {
+  let date = new Date();
+
+  let dateBox = document.getElementById("date");
+
+  if (dateBox) {
+    dateBox.innerHTML = "Today's Date: " + date.toDateString();
+  }
+}
